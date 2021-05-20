77850c7bd830ad4a56a4eb85735e0f15a2903d88
Mahfooz <umahfoosahamed@gmail.com>
2021-05-20 08:37:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParserr = require('body-parser');
+const cors = require('cors');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'1054vvbb',
+    database:'crud',
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParserr.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM movie";
+
+    db.query(sqlSelect,  (err, result) => {
+       res.send(result);
+    });
+});
+
+app.post('/api/insert', (req, res) => {    
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie (name, review) VALUES (?,?)";
+
+    db.query(sqlInsert, [movieName, movieReview] , (err, result) => {
+       console.log(result);
+    })
+});
+    
+
+
+app.listen(3001, () => {
+    console.log('Run On Port 3001');
+});
+
